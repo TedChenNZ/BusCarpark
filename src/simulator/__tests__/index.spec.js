936484292238b05dfd776f9a942f95ddd533a98e
Ted Chen <tedchennz@gmail.com>
2018-03-01 19:22:53 +1300

    Add place, changeDirection, and report functions

--- src/simulator/__tests__/index.spec.js
+++ src/simulator/__tests__/index.spec.js
@@ -1,21 +1,54 @@
-import { COMMANDS, parseInput } from '../';
+import { COMMANDS, parseInput, changeDirection, place } from '../';
 
 describe('simulator', () => {
   describe('parseInput', () => {
-    it('throws an error if invalid input is given', () => {
+    it('should throw an error if invalid input is given', () => {
       expect(() => parseInput('blah')).toThrowError(/Invalid/);
     });
-    it('throws an error if a number is given', () => {
+    it('should throw an error if a number is given', () => {
       expect(() => parseInput(1)).toThrowError(/Invalid/);
     });
-    it('throws an error if undefined is given', () => {
+    it('should throw an error if undefined is given', () => {
       expect(() => parseInput(undefined)).toThrowError(/Invalid/);
     });
-    it('throws an error if null is given', () => {
+    it('should throw an error if null is given', () => {
       expect(() => parseInput(null)).toThrowError(/Invalid/);
     });
-    it('returns the MOVE command if given MOVE ', () => {
+    it('should return the MOVE command if given MOVE', () => {
       expect(parseInput('MOVE')).toEqual(COMMANDS.MOVE);
+    });
+  });
+
+  describe('changeDirection', () => {
+    it('should change the direction to WEST if given a bus facing NORTH and the LEFT command', () => {
+      const bus = {
+        x: 0,
+        y: 0,
+        f: 'NORTH',
+      };
+      const newBus = changeDirection(bus, COMMANDS.LEFT);
+      expect(newBus.f).toEqual('WEST');
+      expect(newBus.x).toEqual(0);
+      expect(newBus.y).toEqual(0);
+    });
+    it('should change the direction to SOUTH if given a bus facing EAST and the RIGHT command', () => {
+      const bus = {
+        x: 2,
+        y: 3,
+        f: 'EAST',
+      };
+      const newBus = changeDirection(bus, COMMANDS.RIGHT);
+      expect(newBus.f).toEqual('SOUTH');
+      expect(newBus.x).toEqual(2);
+      expect(newBus.y).toEqual(3);
+    });
+  });
+
+  describe('place', () => {
+    expect(place(3, 2, 'SOUTH')).toEqual({
+      x: 3,
+      y: 2,
+      f: 'SOUTH',
     });
   });
 });
--- src/simulator/index.js
+++ src/simulator/index.js
@@ -1,3 +1,6 @@
+import { clone } from 'ramda';
+import { getCardinal } from './cardinal';
+
 export const COMMANDS = {
   PLACE: 'PLACE',
   MOVE: 'MOVE',
@@ -16,6 +19,29 @@ export function parseInput(input) {
     }
   }
   throw new Error('Invalid input');
+}
+
+export function place(x, y, f) {
+  return {
+    x,
+    y,
+    f,
+  };
+}
+
+export function changeDirection(bus, direction) {
+  const newBus = clone(bus);
+  if (direction === COMMANDS.LEFT) {
+    newBus.f = getCardinal(bus.f).left.name;
+  }
+  if (direction === COMMANDS.RIGHT) {
+    newBus.f = getCardinal(bus.f).right.name;
+  }
+  return newBus;
+}
+
+export function report(bus) {
+  console.log(`${bus.x},${bus.y},${bus.f}`);
 }
 
 export default parseInput;

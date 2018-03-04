4988b6014b11469637fb835ef8d07b0dce254375
Ted Chen <tedchennz@gmail.com>
2018-03-04 23:07:07 +1300

    Add more tests

--- src/components/Input/__tests__/Input.spec.js
+++ src/components/Input/__tests__/Input.spec.js
@@ -22,18 +22,37 @@ describe('Input', () => {
   })
   
   it('should call update state.input when text is typed', () => {
-    const left = jest.fn();
-    const right = jest.fn();
-    const move = jest.fn();
-    const place = jest.fn();
-    const wrapper = mount(<Input left={left} right={right} move={move} place={place} />);
     wrapper.find('input').simulate('change', { target: { value: 'LEFT' } });
     expect(wrapper.state('input')).toEqual('LEFT');
   });
-  // it('should call left when left is typed', () => {
-  //   const left = jest.fn(); 
-  //   const wrapper = mount(<Input left={left} />);
-  //   wrapper.find('#input').simulate('change', { taget: { value: 'LEFT' } });
-
-  // });
+  it('should call left when LEFT is typed and submitted', () => {
+    wrapper.find('input').simulate('change', { target: { value: 'LEFT' } });
+    wrapper.find('form').simulate('submit');
+    expect(left).toHaveBeenCalled();
+  });
+  it('should call right when RIGHT is typed and submitted', () => {
+    wrapper.find('input').simulate('change', { target: { value: 'RIGHT' } });
+    wrapper.find('form').simulate('submit');
+    expect(right).toHaveBeenCalled();
+  });
+  it('should call move when MOVE is typed and submitted', () => {
+    wrapper.find('input').simulate('change', { target: { value: 'MOVE' } });
+    wrapper.find('form').simulate('submit');
+    expect(move).toHaveBeenCalled();
+  });
+  it('should call place when PLACE is typed and submitted', () => {
+    wrapper.find('input').simulate('change', { target: { value: 'PLACE 0,0,NORTH' } });
+    wrapper.find('form').simulate('submit');
+    expect(place).toHaveBeenCalled();
+  });
+  it('should reset input when submitted', () => {
+    wrapper.find('input').simulate('change', { target: { value: 'anythingH' } });
+    wrapper.find('form').simulate('submit');
+    expect(wrapper.state('input')).toEqual('');
+  });
+  it('should show an error when incorrect text is submitted', () => {
+    wrapper.find('input').simulate('change', { target: { value: 'anything' } });
+    wrapper.find('form').simulate('submit');
+    expect(wrapper.html()).toContain('Invalid command');
+  });
 });
